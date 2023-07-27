858c77ef6313a4d150a198caa0ece2c425519000
JoaqinDLH <134020388+JoaqinDLH@users.noreply.github.com>
2023-07-26 21:26:29 -0400

    commit and push by joaking

--- src/main/java/com/example/trabajom5tg1/dao/LoginDAO.java
+++ src/main/java/com/example/trabajom5tg1/dao/LoginDAO.java
@@ -3,23 +3,25 @@ package com.example.trabajom5tg1.dao;
 import com.example.trabajom5tg1.conexion.Conexion;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
-import java.sql.Statement;
 
 public class LoginDAO {
     public static boolean validarUsuario(String usuario, String contrasena){
         boolean validarUsuario = false;
-        Statement stm = null;
+        PreparedStatement ps = null;
         Connection con = null;
 
-        String sql = "SELECT * FROM Usuarios WHERE nombreUsuario = '"+usuario+"' AND password = '"+contrasena+"'";
-        String tipoUsuario = "SELECT tipo FROM usuario WHERE nombreUsuario = '"+usuario+"' AND password = '"+contrasena+"'";
+        String sql = "SELECT * FROM Usuarios WHERE nombreUsuario = ? AND password = ?";
         try {
             con = Conexion.conectar();
-            stm = con.createStatement();
-            stm.executeQuery(sql);
-            validarUsuario = true;
-            stm.close();
+            ps = con.prepareStatement(sql);
+            ps.setString(1, usuario);
+            ps.setString(2, contrasena);
+            ResultSet rs = ps.executeQuery();
+            validarUsuario = rs.next(); // Verificar si se encontró algún resultado
+            rs.close();
+            ps.close();
             con.close();
         } catch (Exception e) {
             System.out.println("Error: Clase LoginDAO, método validarUsuario");
@@ -27,25 +29,28 @@ public class LoginDAO {
         }
         return validarUsuario;
     }
+
     public static String tipoUsuario(String usuario) {
         String tipoUsuario = "";
-        Statement stm = null;
+        PreparedStatement ps = null;
         Connection con = null;
 
-        String sql = "SELECT tipo FROM Usuarios WHERE nombreUsuario = '" + usuario + "'";
+        String sql = "SELECT tipo FROM Usuarios WHERE nombreUsuario = ?";
         try {
             con = Conexion.conectar();
-            stm = con.createStatement();
-            ResultSet rs = stm.executeQuery(sql); // Ejecutar la consulta y obtener el ResultSet
+            ps = con.prepareStatement(sql);
+            ps.setString(1, usuario);
+            ResultSet rs = ps.executeQuery();
             if (rs.next()) {
-                tipoUsuario = rs.getString("tipo"); // Obtener el valor del campo "tipo"
+                tipoUsuario = rs.getString("tipo");
             }
             rs.close();
-            stm.close();
+            ps.close();
             con.close();
         } catch (Exception e) {
             System.out.println("Error: Clase LoginDAO, método tipoUsuario");
             e.printStackTrace();
         }
         return tipoUsuario;
-    }}
+    }
+}
